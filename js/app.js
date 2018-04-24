bccbf62858de31f33e98cb3e0dff4b83de758d45
Ankit Karnany <ankitkarnany@gmail.com>
2018-04-24 21:26:10 +0530

    basic ui with third party slider implemented

---
+++ js/app.js
@@ -0,0 +1,11 @@
+angular.module("App", ['ui-rangeSlider']);
+
+angular.module("App").controller("mainCtrl", function ($scope) {
+
+  $scope.minPrice = 0;
+  $scope.maxPrice = 10000;
+
+  $scope.userMinPrice = $scope.minPrice;
+  $scope.userMaxPrice = $scope.maxPrice;
+
+});
