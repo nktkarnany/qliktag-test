5101dc26288b6d95d21d8c489fe7b437ffb326e2
Ankit Karnany <ankitkarnany@gmail.com>
2018-04-25 09:40:04 +0530

    autocomplete directive implemented for input elements

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 var app = angular.module("App", ['ui-rangeSlider']);
 
-app.controller("mainCtrl", function ($scope) {
+app.controller("mainCtrl", ['$scope', 'Flight', '$filter', function ($scope, Flight, $filter) {
 
-  $scope.minPrice = 0;
-  $scope.maxPrice = 10000;
-
-  $scope.userMinPrice = $scope.minPrice;
-  $scope.userMaxPrice = $scope.maxPrice;
+  // Intializations
+  $scope.price = {
+    min: 0,
+    max: 10000,
+    userMin: 2000,
+    userMax: 7000
+  };
   
-  $scope.showValues = true;
+  // Populating dates
+  $scope.allDates = Flight.allDates;
+  // Populating Cities
+  $scope.allCities = Flight.allCities;
+  
+  $scope.filter = {
+    depDate: ''
+  };
+  
+  $scope.search = function() {
+    console.log($scope.filter);
+  }
   
   $scope.searched_flights = [
     {
@@ -51,9 +64,94 @@ app.controller("mainCtrl", function ($scope) {
     }
   ];
 
-});
+}]);
 
-app.directive('flightCard', function () {
+app.service('Flight', [function () {
+  
+  this.allDates = ['2nd May', '4th May'];
+  this.allCities = [
+    {
+      name: "Pune",
+      code: "PNQ"
+    }, {
+      name: "Delhi",
+      code: "DEL"
+    }
+  ];
+  
+  var allFlights = [
+    {
+      date: "2nd May",
+      source: {
+        city: {
+          name: "Pune",
+          code: "PNQ"
+        },
+        time: "10:00AM"
+      },
+      destination: {
+        city: {
+          name: "Delhi",
+          code: "DEL"
+        },
+        time: "12:00PM"
+      },
+      flight_no: "AOL-31",
+      price: 4500
+    }, {
+      date: "4th May",
+      source: {
+        city: {
+          name: "Delhi",
+          code: "DEL"
+        },
+        time: "3:00PM"
+      },
+      destination: {
+        city: {
+          name: "Pune",
+          code: "PNQ"
+        },
+        time: "6:00PM"
+      },
+      flight_no: "AOL-33",
+      price: 4700
+    }
+  ];
+  
+}]);
+
+app.directive('autocomplete', ['$timeout', '$filter', function ($timeout, $filter) {
+  return {
+    restrict: 'E',
+    scope: {
+      placeholder: '@',
+      term: '=val',
+      values: '=',
+      isCity: '@'
+    },
+    controller: function ($scope) {
+      $scope.search = function (s) {
+        if (s != '')
+          $scope.items = $filter('filter')($scope.values, s);
+        else
+          $scope.items = [];
+      }
+      $scope.selectItem = function (item) {
+        $scope.term = item;
+        $scope.items = [];
+      }
+    },
+    template: `<div class="dropdown ng-class:{'open':items.length > 0}">
+                  <input type="text" class="form-control" placeholder="{{placeholder}}" aria-haspopup="true" aria-expanded="false" ng-model="term" ng-change="search(term)">
+                  <ul class="dropdown-menu" aria-labelledby="autocomplete">
+                    <li ng-repeat="item in items" ng-click="selectItem(item)"><a href=""><span ng-if="isCity">{{item.name}} - {{item.code}}</span><span ng-if="!isCity">{{item}}</span></a></li>
+                  </ul>
+                </div>`
+    };
+}]);
+
+app.directive('flightCard', [function () {
   return {
     restrict: 'E',
     scope: {
@@ -103,4 +201,4 @@ app.directive('flightCard', function () {
         </div>
       </div>`
   };
-});
+}]);
